e4fa11221379e4cb6ff35acd80b4db394b20152b
Esteban <esteban.delapena@mangoodev.com>
2021-07-24 16:10:43 -0300

    Make admin add product works with MongoDB

--- util/database.js
+++ util/database.js
@@ -1,17 +1,26 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://estebandlp:6DbMV0FNxfu2ZboI@cluster0.e8dgv.mongodb.net/nodejscourse?retryWrites=true&w=majority"
+    "mongodb+srv://estebandlp:6DbMV0FNxfu2ZboI@cluster0.e8dgv.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("connected");
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) return _db;
+  throw 'No database found!.'
+}
+
+exports.getDb = getDb; 
+exports.mongoConnect = mongoConnect;
