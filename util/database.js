493ae293ea9e45a8c27c692895d9075af1df0334
Esteban <esteban.delapena@mangoodev.com>
2021-07-24 15:19:07 -0300

    Fix issue to connect with MongoDb

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://estebandlp:<6DbMV0FNxfu2ZboI>@cluster0.e8dgv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://estebandlp:6DbMV0FNxfu2ZboI@cluster0.e8dgv.mongodb.net/nodejscourse?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("connected");
