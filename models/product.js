2b55bc028d8e572c0abdfff35b46559d5e581f4a
Esteban de la Peña <estebanddlp@gmail.com>
2021-07-25 20:39:15 -0300

    Migrate product detail view

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const getDb = require('../util/database').getDb;
+const mongoDb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -10,7 +11,41 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection('product').insertOne(this).then(result => { console.log(result) }).catch(err => { console.log(err) });
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static findById(productId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongoDb.ObjectId(productId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
