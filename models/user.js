aae23a80d0a376dbff7e2263f295b6e55749f2f5
Esteban de la Peña <estebanddlp@gmail.com>
2021-07-31 10:53:57 -0300

    Migrate add orders and orders detail

--- models/user.js
+++ models/user.js
@@ -82,6 +82,46 @@ class User {
     });
   }
 
+  addOrder() {
+    const db = getDb();
+
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongoDb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new mongoDb.ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+
+    return db
+      .collection("orders")
+      .find({ "user._id": this._id })
+      .toArray()
+      .then((orders) => {
+        return orders;
+      })
+      .catch((err) => console.log(err));
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
