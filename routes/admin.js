f7de75b0893a889183d48aa7bfa71cb700bd4db2
Esteban de la Peña <40476810+estebandlp@users.noreply.github.com>
2021-08-15 11:13:03 -0300

    Issue-14: add validations (#31)
    * Add validations to login and signup forms
    * Add add-product and edit-product validations

--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,39 @@ const express = require("express");
 
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
+const { body } = require("express-validator/check");
 
 const router = express.Router();
 
 router.get("/add-product", isAuth, adminController.getAddProduct);
 
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  isAuth,
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isAlphanumeric().isLength({ min: 5, max: 400 }).trim(),
+  ],
+  adminController.postAddProduct
+);
 
 router.get("/products", adminController.getProducts);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  isAuth,
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isAlphanumeric().isLength({ min: 5, max: 400 }).trim(),
+  ],
+  adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
