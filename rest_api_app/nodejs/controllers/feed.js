d2ef7dc1f7af727df3fcb833c79c8a22dbc3b58b
Esteban de la Peña <estebanddlp@gmail.com>
2021-08-26 20:16:26 -0300

    Working on fetching list o feeds

--- rest_api_app/nodejs/controllers/feed.js
+++ rest_api_app/nodejs/controllers/feed.js
@@ -1,14 +1,22 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "Dummy data" }],
+    posts: [
+      {
+        title: "First Post",
+        content: "Dummy data",
+        imageUrl: "images/duck.jfif",
+        creator: {
+          name: "Tebuso",
+        },
+        createdAt: new Date()
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-
-  console.log(req.body);
 
   // Create post in db
 
