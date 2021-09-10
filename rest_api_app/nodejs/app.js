e3eb5c12b1af1af9d1beb4d6b88648ab35c67680
Esteban de la Peña <40476810+estebandlp@users.noreply.github.com>
2021-09-10 20:21:35 -0300

    Issue-18: Rest API Practical app (#41)

---
+++ rest_api_app/nodejs/app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const bodyParser = require("body-parser");
+const databaseConnect = require("./util/database").databaseConnect;
+const path = require("path");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({ message: message, data: data });
+});
+
+databaseConnect()
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
