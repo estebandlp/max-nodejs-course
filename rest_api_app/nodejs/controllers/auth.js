70b9f2aa1eba6b9a4d892ac61bbb7b4070d7c0df
Esteban de la Peña <40476810+estebandlp@users.noreply.github.com>
2021-09-19 12:22:09 -0300

    Issue-20: Add websocket using socket-io (#43)

--- rest_api_app/nodejs/controllers/auth.js
+++ rest_api_app/nodejs/controllers/auth.js
@@ -4,15 +4,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.signup = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed.");
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
-
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed.");
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+
     const hashedPw = await bcrypt.hash(req.body.password, 12);
 
     const user = new User({
@@ -22,9 +23,9 @@ exports.signup = async (req, res, next) => {
     });
 
     await user.save();
-
-    res.status(201).json({ message: "User created!", userId: result._id });
+    res.status(201).json({ message: "User created!", userId: user._id });
   } catch (err) {
+    console.log(err);
     if (!err.statusCode) {
       err.statusCode = 500;
     }
@@ -35,14 +36,15 @@ exports.signup = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     let loadedUser = await User.findOne({ email: req.body.email });
-
     if (!loadedUser) {
       const error = new Error("A user with this email could not be found.");
       error.statusCode = 401;
       throw error;
     }
 
-    const isEqual = await bcrypt.compare(req.body.password, user.password);
+    const isEqual = await bcrypt.compare(req.body.password, loadedUser.password);
+
+    console.log(isEqual);
 
     if (!isEqual) {
       const error = new Error("Wrong password!");
--- rest_api_app/nodejs/controllers/feed.js
+++ rest_api_app/nodejs/controllers/feed.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { validationResult } = require("express-validator/check");
 const Post = require("../models/post");
 const User = require("../models/user");
+const io = require("./../socket");
 
 exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -10,8 +11,11 @@ exports.getPosts = async (req, res, next) => {
 
   try {
     const totalItems = await Post.find().countDocuments();
-
-    const posts = await Post.find().skip((currentPage - 1) * perPage);
+    const posts = await Post.find()
+      .populate("creator")
+      .sort({ createdAt: -1 })
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
 
     res.status(200).json({
       message: "Post fetched!",
@@ -27,20 +31,20 @@ exports.getPosts = async (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed.");
-    error.statusCode = 422;
-    throw error;
-  }
-  if (!req.file) {
-    const error = new Error("No image provided.");
-    error.statusCode = 422;
-    throw error;
-  }
-
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed.");
+      error.statusCode = 422;
+      throw error;
+    }
+    if (!req.file) {
+      const error = new Error("No image provided.");
+      error.statusCode = 422;
+      throw error;
+    }
+
     let post = new Post({
       title: req.body.title,
       content: req.body.content,
@@ -53,6 +57,11 @@ exports.createPost = async (req, res, next) => {
     let creator = await User.findById(req.userId);
     creator.posts.push(post);
     await creator.save();
+
+    io.getIO().emit("posts", {
+      action: "create",
+      post: { ...post._doc, creator: { _id: req.userId, name: creator.name } },
+    });
 
     res.status(201).json({
       message: "Post created!",
@@ -93,19 +102,19 @@ exports.getPost = async (req, res, next) => {
 };
 
 exports.updatePost = async (req, res, next) => {
-  let imageUrl = req.body.image;
-
-  if (req.file) {
-    imageUrl = req.file.path.replace("\\", "/");
-  }
-  if (!imageUrl) {
-    const error = new Error("No file picked.");
-    error.statusCode = 422;
-    throw error;
-  }
-
   try {
-    const post = await Post.findById(req.params.postId);
+    let imageUrl = req.body.image;
+
+    if (req.file) {
+      imageUrl = req.file.path.replace("\\", "/");
+    }
+    if (!imageUrl) {
+      const error = new Error("No file picked.");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const post = await Post.findById(req.params.postId).populate("creator");
 
     if (!post) {
       const error = new Error("Post not found.");
@@ -113,7 +122,7 @@ exports.updatePost = async (req, res, next) => {
       throw error;
     }
 
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error("Not authorized!");
       error.statusCode = 403;
       throw error;
@@ -127,7 +136,9 @@ exports.updatePost = async (req, res, next) => {
     post.content = req.body.content;
     post.imageUrl = imageUrl;
 
-    await post.save();
+    const result = await post.save();
+
+    io.getIO().emit("posts", { action: "update", post: result });
 
     res.status(200).json({ message: "Done!", post: result });
   } catch (err) {
@@ -162,6 +173,11 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
+
+    io.getIO().emit("posts", {
+      action: "delete",
+      post: postId,
+    });
 
     res.status(200).json({ message: "Delete done!" });
   } catch (err) {
